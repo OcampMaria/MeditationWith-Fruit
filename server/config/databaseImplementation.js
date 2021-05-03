02cde98669ef139ebc841ab91c8f4346c13e1f05
Ocampo <mariaocampo0513@gmail.com>
2021-05-02 21:02:14 -0700

    deploying

--- server/config/databaseImplementation.js
+++ server/config/databaseImplementation.js
@@ -1,13 +1,18 @@
-const mongoose 			 = require('mongoose');
-const configDB = require('./databaseCreds');
-mongoose.connect(configDB.url, { useNewUrlParser: true });
+const mongoose = require("mongoose");
+const configDB = require("./databaseCreds");
+mongoose.connect(configDB.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 //Get the default connection
 const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-db.once("open", function() {
+db.once("open", function () {
   console.log("Mongoose connection successful.");
 });
