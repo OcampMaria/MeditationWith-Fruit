d4dc8d826bfebe9e4ce133b01fd937fd15fd7bf1
Ocampo <mariaocampo0513@gmail.com>
2021-06-07 18:02:42 -0700

    added quiz component

--- app/components/Quiz.js
+++ app/components/Quiz.js
@@ -1,77 +1,123 @@
 import React, { Component } from "react";
+import { QuizData } from "./QuizData";
+import { Fruits } from "./QuizData";
 import logo from "./images/orange.png";
 import { Link } from "react-router-dom";
 import Footer from "./children/Footer";
-import * as Survey from "survey-react";
-
-import "survey-react/survey.css";
 
 require("./main.css");
-require("./styles/about.css");
+require("./styles/quiz.css");
 
-// Creating the Main component
-export default class About extends Component {
+
+export class Quiz extends Component {
+  // rconst
   constructor(props) {
     super(props);
-    this.state = { isCompleted: false };
-    this.onCompleteComponent = this.onCompleteComponent.bind(this);
+
+    this.state = {
+      userAnswer: null,
+      currentIndex: 0,
+      options: [],
+      quizEnd: false,
+      score: 0,
+      disabled: true,
+      fruit: "",
+    };
   }
 
-  onCompleteComponent() {
-    this.setState({ isCompleted: true });
+  loadQuiz = () => {
+    const { currentIndex } = this.state;
+
+    this.setState(() => {
+      return {
+        question: QuizData[currentIndex].question,
+        fruit: Fruits[currentIndex].name,
+        options: QuizData[currentIndex].options,
+        // answer: QuizData[currentIndex].answer,
+      };
+    });
+  };
+
+  nextQuestionHander = () => {
+    const { userAnswer, score } = this.state;
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+    });
+    // if sstatements. if useranswer is === to answer, then increase score by one.
+
+    //Check if correct answer and increment score
+    if (userAnswer) {
+      this.setState({
+        score: score + 1,
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuiz();
   }
+
+  checkAnswer = (answer) => {
+    this.setState({
+      userAnswer: answer,
+      disabled: false,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentIndex } = this.state;
+    if (this.state.currentIndex != prevState.currentIndex) {
+      this.setState(() => {
+        return {
+          // question: QuizData[currentIndex].question,
+          fruit: Fruits[currentIndex].name,
+          // options: QuizData[currentIndex].options,
+
+          //   answer: QuizData[currentIndex].answer,
+        };
+      });
+    }
+  }
+
+  finishHandler = () => {
+    if (this.state.currentIndex === Fruits.length - 1) {
+      this.setState({
+        quizEnd: true,
+      });
+    }
+  };
 
   render() {
-    let json = {
-      questions: [
-        {
-          type: "checkbox",
-          name: "car",
-          title: "What car are you driving?",
-          isRequired: true,
-          hasSelectAll: true,
-          hasNone: true,
-          noneText: "None of the above",
-          colCount: 4,
-          choicesOrder: "asc",
-          choices: [
-            "Apple",
-            "Apricots",
-            "Banana",
-            "Blueberries",
-            "Cantaloupe",
-            "Cherries",
-            "Clementines",
-            "Grapes",
-            "Honeydew",
-            "Kiwi",
-            "Mango",
-            "Nectarines",
-            "Orange",
-            "Peachers",
-            "Pears",
-            "Pineapple",
-            "Plums",
-            "Pomegranate",
-            "Starfruit",
-            "Strawberries",
-          ],
-        },
-      ],
+    const { question, options, currentIndex, userAnswer, quizEnd, fruit } =
+      this.state;
+    const numbers = function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
     };
-    const surveyRender = !this.state.isCompleted ? (
-      <Survey.Survey
-        json={json}
-        showCompletedPage={false}
-        onComplete={this.onCompleteComponent}
-      />
-    ) : null;
-    const onCompleteComponent = this.state.isCompleted ? (
-      <div>The component after onComplete event</div>
-    ) : null;
+
+    if (quizEnd) {
+      return (
+        <div>
+          <h2>
+            Based on your responses, the fruit that will be most effective for
+            this program is __.
+          </h2>
+          <p>
+            Your first step is to go to the store and buy 7 servings of that
+            fruit so you’ll be ready to eat this week!
+          </p>
+          {/* <ul>
+            {fruits.map((item, index) => (
+              <li className="ui floating message options" key={index}>
+                {item.answer}
+              </li>
+            ))}
+          </ul> */}
+        </div>
+      );
+    }
 
     return (
-      <div className="container full-height-grow">
+      <div>
         <header className="main-header">
           <a href="" className="brand-logo">
             <img className="logo-secondary" src={logo} alt="" />
@@ -88,13 +134,42 @@ export default class About extends Component {
           </nav>
         </header>
 
-        <div>hello
-     
-        </div>
+        <h1>{fruit}</h1>
       
-
-        <Footer />
+        <h2>{question}</h2>
+        <span>{`Question ${currentIndex + 1} of ${Fruits.length - 1} `}</span>
+        {options.map((option) => (
+          <p
+            key={option + numbers}
+            className={`ui floating message options
+            ${userAnswer === option ? "selected" : null}
+            `}
+            onClick={() => this.checkAnswer(option)}
+          >
+            {option}
+          </p>
+        ))}
+        {currentIndex < Fruits.length - 1 && (
+          <button
+            className="ui inverted button"
+            disabled={this.state.disabled}
+            onClick={this.nextQuestionHander}
+          >
+            Next Question
+          </button>
+        )}
+        {currentIndex === Fruits.length - 1 && (
+          <button
+            className="ui inverted button"
+            disabled={this.state.disabled}
+            onClick={this.finishHandler}
+          >
+            Finish
+          </button>
+        )}
       </div>
     );
   }
 }
+
+export default Quiz;
---
+++ server/models/Fruits.js
@@ -0,0 +1,45 @@
+// 'use strict';
+
+// // app/models/fruits.js
+// // load the things we need
+// var mongoose = require('mongoose');
+// var bcrypt   = require('bcrypt-nodejs');
+
+// // define the schema for our user model
+// var fruitsSchema = mongoose.Schema({
+
+//     Fruit: {
+//         type: String,
+//         min: [1, 'Too few characters'],
+//         max: 100,
+//         required: [true, 'Please enter a username.']
+//     },
+//     email: {
+//         type: String,
+//         min: [3, 'Please enter an email in the correct format'],
+//         required: [true, 'Please enter an email']
+//     },
+//     password: {
+//         type: String,
+//         min: [8, 'Your password must be at least 8 characters large'],
+//         required: [true, 'Please enter a password.']
+//     },
+//     assignedFruit: {
+//         type: String
+//     }
+
+// });
+
+// // methods ======================
+// // generating a hash
+// userSchema.methods.generateHash = function(password) {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// };
+
+// // checking if password is valid
+// userSchema.methods.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.password);
+// };
+
+// // create the model for users and expose it to our app
+// module.exports = mongoose.model('Fruits', fruitsSchema);
