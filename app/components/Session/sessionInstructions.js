76c323a8b7b38db8556dbf45f7a45e6442cd9ff4
Ocampo <mariaocampo0513@gmail.com>
2021-07-30 11:16:48 -0700

    Instructions and heroku bug

--- app/components/Session/sessionInstructions.js
+++ app/components/Session/sessionInstructions.js
@@ -3,6 +3,7 @@ import logo from "../images/orange.png";
 import { slideData } from "./sessionSlides";
 import { Link } from "react-router-dom";
 import Footer from "../children/Footer";
+import Nav from "../children/Nav";
 
 
 // require("./main.css");
@@ -13,7 +14,7 @@ export default function Instructions(props) {
   const [currentIndex, setcurrentIndex] = useState(0);
   const [paragraphs, setparagraphs] = useState("");
   const [image, setimage] = useState();
-  
+
   const loadSlides = () => {
     setparagraphs(slideData[currentIndex].paragraph);
     setimage(slideData[currentIndex].imagePath);
@@ -28,72 +29,68 @@ export default function Instructions(props) {
   });
 
   return (
-    <div className="container full-height-grow">
-      <header className="main-header">
-        <a href="" className="brand-logo">
-          <img className="logo-secondary" src={logo} alt="" />
-        </a>
-        <nav className="main-nav">
-          <ul>
-            <li className="nav-items secondary-nav">
-              <Link to={"/"}>Home</Link>
-            </li>
+    <div className="">
+      <Nav />
+      <div className=" component-container">
+    
+      {/* home, log in */}
 
-            <li className="nav-items secondary-nav">
-              <Link to={"/login"}>Log In</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+        {/* <section className="intro-section"> */}
+          {/* <div className="max-with"> */}
+            {/* <div> */}
+              <h3 className="title-secondary">
+                - Daily Session Instructions - 
+               <br /> 
+               <span className="accent-text"> Welcome to today’s relaxation session!</span>
+               
+              </h3>
+              {/* <h1 className="title-secondary">
+               
+              </h1> */}
+            {/* </div> */}
 
-      <section className="intro-section">
-        <div className="max-with">
-          <div>
-          <h3 className="title-secondary">- Daily Session Instructions -</h3>
-          <h1 className="title-secondary">Welcome to today’s relaxation session!</h1>
-          </div>
-          
-
-          <div className="carousel scrollmenu">
-            <div className="owl-item" id="instruction1">
-              <div className="card">
-                <div className="box" id="one">
-                  <p className="text">{paragraphs}</p>
-                  <div className="img ilustrations">
-                    <a href="" className="brand-logo">
-                      <img
-                        className="logo-secondary ilustration"
-                        src={image}
-                        alt=""
-                      />
-                    </a>
+            <div className="carousel scrollmenu">
+              <div className="owl-item" id="instruction1">
+                <div className="card">
+                  <div className="box" id="one">
+                    <p className="text">{paragraphs}</p>
+                    <div className="img ilustrations">
+                      <a href="" className="brand-logo">
+                        <img
+                          className="logo-secondary ilustration"
+                          src={image}
+                          alt=""
+                        />
+                      </a>
+                    </div>
                   </div>
                 </div>
+                {currentIndex < slideData.length - 1 && (
+                  <Link to="#" className="register">
+                    <button
+                      type="button"
+                      className="btn btn-register"
+                      onClick={handleClick}
+                    >
+                      NEXT
+                    </button>
+                  </Link>
+                )}
+
+                {currentIndex === slideData.length - 1 && (
+                  <Link to="/audioplayer" className="register">
+                    <button type="button" className="btn btn-register">
+                      NEXT
+                    </button>
+                  </Link>
+                )}
               </div>
-              {currentIndex < slideData.length - 1 && (
-                <Link to="#" className="register">
-                  <button
-                    type="button"
-                    className="btn btn-register"
-                    onClick={handleClick}
-                  >
-                    NEXT
-                  </button>
-                </Link>
-              )}
-
-              {currentIndex === slideData.length - 1 && (
-                <Link to="/audioplayer" className="register">
-                  <button type="button" className="btn btn-register">
-                    NEXT
-                  </button>
-                </Link>
-              )}
             </div>
-          </div>
-        </div>
-      </section>
+          {/* </div> */}
+        {/* </section> */}
 
+     
+      </div>
       <Footer />
     </div>
   );
