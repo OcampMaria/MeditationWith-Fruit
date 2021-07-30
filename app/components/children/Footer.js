8534bcbfb5a44d656a3445d63bd7ae6b916a5ce5
Ocampo <mariaocampo0513@gmail.com>
2021-07-29 21:23:47 -0700

    footer and heroku bug

--- app/components/children/Footer.js
+++ app/components/children/Footer.js
@@ -5,37 +5,34 @@ require("./footer.css");
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <footer className="main-footer">
-          <div className="Footer-container">
-            <nav className="footer-nav">
-              <ul>
-                <li>
-                  <a href="#">About Us</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </nav>
+      <div className="footer-container">
+        <div>
+          <ul className="wrap">
+            <li className="list">
+              <a className="item">DiSH Lab</a>
+            </li>
+            <li>
+              <a href="">About Us</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
 
-            <nav className="footer-nav">
-              <ul>
-                <li>
-                  <a href="#">DiSH Lab</a>
-                </li>
-                <li>
-                  <a href="#">Resources</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-
-          <p className="footer-font">
-          Copyright &copy; 6-minutes with fruit 2021
-        </p>
-        </footer>
-        
+        <div>
+          <ul className="wrap">
+            <li>
+              <a href="">Resources</a>
+            </li>
+            <li>
+              <a href="">Facebook</a>
+            </li>
+            <li>
+              <a href="">Facebook 👋</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
