d4fae9b6d2a02016714ec6a900c0d74ed4748c67
Ocampo <mariaocampo0513@gmail.com>
2021-07-29 21:33:43 -0700

    profile and heroku bug

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,79 +3,51 @@ import Nav from "./children/Nav";
 import logo from "./images/orange.png";
 import { Link } from "react-router-dom";
 import Footer from "./children/Footer";
-import img1 from "./images/fruits.jpg";
 
 // require("./main.css");
 require("./styles/profile.css");
 
-// Creating the Main component
 export default function Profile() {
+  return (
+    <div className=" full-height-grow">
+      <Nav />
+      <div className="">
+        {/* <section className="profile-section"> */}
+          <div className="max-with">
+            <div className="session">
+              <h3 className="title-secondary">- Welcome Back {}! -</h3>
 
-    return (
-      <div>
-        <Nav />
-        <div className=" full-height-grow">
-          <section className="profile-section">
-            <div className="max-with">
-              <div className="session">
-                <h3 className="title-secondary">- Welcome Back {}! -</h3>
-
-                <div>
-                  <p className="subtitle-profile">New Session</p>
-                  <Link to="/sessioninstructions" className="new-session">
-                    <button type="button" className="  btn-session">
-                      +
-                    </button>
-                  </Link>
-                </div>
-              </div>
-
-              <div className="wrap">
-                <div className="one">
-                  <div className="card">
-                    <div className="box">
-                      <p className="text">kkk</p>
-                      <div className="img ">
-                        <a href="" className="">
-                          <img className=" ilustration" src={img1} />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                {/* <div className="one">
-                <div className="card">
-                  <div className="box">
-                    <p className="text">kkk</p>
-                    <div className="img ">
-                      <a href="" className="">
-                        <img className=" ilustration" src={img1} />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
-
-                {/* <div className="one">
-                <div className="card">
-                  <div className="box">
-                    <p className="text">kkk</p>
-                    <div className="img ">
-                      <a href="" className="">
-                        <img className=" ilustration" src={img1} />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
+              <div>
+                <p className="subtitle-profile">New Session</p>
+                <Link to="/sessioninstructions" className="new-session">
+                  <button type="button" className="  btn-session">
+                    +
+                  </button>
+                </Link>
               </div>
             </div>
-          </section>
 
-          <Footer />
-        </div>
+            {/* <div className="wrap">
+              <div className="one">
+                <div className="card">
+                  <div className="box">
+                    <p className="text">kkk</p>
+                    <div className="img ">
+                      <a href="" className="">
+                        <img className=" ilustration" src={img1} />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+            </div> */}
+          </div>
+        {/* </section> */}
+
+        
       </div>
-    );
-  
+      <Footer />
+    </div>
+  );
 }
